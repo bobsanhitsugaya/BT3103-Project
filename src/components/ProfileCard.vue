f1e13f5b853ce67a2f7438dd34ce39decde1b197
cheahcha <61797648+cheahcha@users.noreply.github.com>
2020-03-30 18:36:28 +0800

    Added New Student Requests
    - separated into profile card, message card, requests cards into components

---
+++ src/components/ProfileCard.vue
@@ -0,0 +1,107 @@
+<template
+  ><v-container fluid grid-list-lg>
+    <v-layout row wrap>
+      <v-flex md>
+        <h2>NUSTutors</h2>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap>
+      <v-flex md>
+        <v-hover>
+          <v-card
+            slot-scope="{ hover }"
+            :class="`elevation-${hover ? 12 : 2}`"
+            class="profile"
+          >
+            <div class="top-card">
+              <v-avatar size="100" class="avatar">
+                <img
+                  src="https://i.pinimg.com/originals/57/3e/9e/573e9e53ee78e2a88c32d53bd8a5bfd2.jpg"
+                  alt="dog"
+                />
+              </v-avatar>
+            </div>
+
+            <v-card-title primary-title>
+              <div>
+                <h3>{{ name }}</h3>
+                <div>
+                  <h6>{{ course }}</h6>
+                  <body>
+                    Hourly Rate: $ {{ rate }}/hr
+                    <br />
+                    Number of Past Tutees: {{ paststudents }}
+                    <br />
+                    Currently tutoring: {{ currentstudents }} <br />Rating:
+                    <br />
+                    <star-rating
+                      read-only
+                      v-model="rating"
+                      :show-rating="false"
+                      rounded-corners
+                      :star-size="30"
+                      inline
+                    ></star-rating>
+                    <br />
+                    <br />
+                  </body>
+                  <div class="review">
+                    click here to see reviews written by Tan's past tutees
+                  </div>
+                </div>
+              </div>
+            </v-card-title>
+
+            <v-card-actions></v-card-actions>
+          </v-card>
+        </v-hover>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import StarRating from 'vue-star-rating';
+export default {
+  components: {
+    StarRating
+  },
+  data() {
+    return {
+      name: 'Jon Tan',
+      course: 'Year 4 Computer Science',
+      rate: 10,
+      paststudents: 25,
+      currentstudents: 5,
+      rating: 3
+    };
+  }
+};
+</script>
+
+<style>
+h2 {
+  color: #f1ba79;
+  font-weight: bold;
+}
+h3 {
+  margin-top: 2.5rem;
+}
+.avatar {
+  margin: -3.5rem;
+}
+.top-card {
+  height: 8rem;
+  background-color: #f1ba79;
+  display: flex;
+  justify-content: center;
+  align-items: flex-end;
+}
+.review {
+  text-decoration: underline;
+  font-size: 0.8em;
+  padding: 1em 0;
+  color: gray;
+  white-space: normal;
+}
+</style>
