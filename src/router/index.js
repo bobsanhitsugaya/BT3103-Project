a51f272131491124ac7c762b61b1fbbc9c135638
cheahcha <61797648+cheahcha@users.noreply.github.com>
2020-03-30 22:51:54 +0800

    Node module for vue-grd

--- src/router/index.js
+++ src/router/index.js
@@ -1,63 +1,62 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 //import Chat from '../Chat.vue'
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-    {
-        path: '/home',
-        name: 'home',
-        component: () => import('../views/Home'),
-    },
-    {
-        path: '/dashboard',
-        name: 'dashboard',
-        component: () => import('../views/Dashboard/Layout'),
-    },
-    {
-        path: '/chat',
-        name: 'Chat',
-        component: () => import('../Chat.vue'),
-        props: true,
-    },
-    {
-        path: '/login',
-        name: 'login',
-        component: () => import('../views/Landing/loginuser.vue'),
-    },
-    {
-        path: '/register',
-        name: 'register',
-        component: () => import('../views/Landing/Register.vue'),
-    },
-    {
-        path: '/',
-        name: 'landing',
-        component: () => import('../views/Landing/Landing.vue'),
-    },
-    {
-        path: '/search',
-        name: 'search',
-        component: () => import('../views/Search.vue')
-    },
-    {
-        path: '/tutors/:tut',
-        component: () => import('../views/singleBlog.vue')
-    },
-    {
-        path: '/profile',
-        name: 'profile',
-        component: () => import('../views/Profile/Profile.vue')
-    }
-]
+  {
+    path: '/home',
+    name: 'home',
+    component: () => import('../views/Home.vue')
+  },
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('../views/Dashboard/Layout')
+  },
+  {
+    path: '/chat',
+    name: 'Chat',
+    component: () => import('../Chat.vue'),
+    props: true
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Landing/loginuser.vue')
+  },
+  {
+    path: '/register',
+    name: 'register',
+    component: () => import('../views/Landing/Register.vue')
+  },
+  {
+    path: '/',
+    name: 'landing',
+    component: () => import('../views/Landing/Landing.vue')
+  },
+  {
+    path: '/search',
+    name: 'search',
+    component: () => import('../views/Search.vue')
+  },
+  {
+    path: '/tutors/:tut',
+    component: () => import('../views/singleBlog.vue')
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: () => import('../views/Profile/Profile.vue')
+  }
+];
 
 const router = new VueRouter({
-    mode: 'history',
-    base: process.env.BASE_URL,
-    routes
-})
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+});
 
-  export default router
+export default router;
